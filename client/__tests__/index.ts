de044ecd359269f5a550cdd7a63a3fcd2d75b5f9
Peter Shoaf <32045721+DownsizeEngineering@users.noreply.github.com>
2020-03-11 12:29:26 -0700

    test jest functionality

---
+++ client/__tests__/index.ts
@@ -0,0 +1,9 @@
+import React from 'react';
+import { shallow, mount, render } from 'enzyme';
+// import App from '../components/App.jsx';
+
+describe('TEST', ()=>{
+  test('works', ()=>{
+    expect(2).toEqual(2);
+  })
+});

96237613a0fe6ffa21360c0515a068a1afbcaf4b
Peter Shoaf <32045721+DownsizeEngineering@users.noreply.github.com>
2020-03-13 15:18:06 -0700

    create seed script

---
+++ server/seed.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+const mongoose = require('mongoose');
+mongoose.Promise = Promise;
+
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
+mongoose.set('useUnifiedTopology', true);
+mongoose.connect('mongodb://localhost/images');
+
+const imageSchema = new mongoose.Schema({
+  id: Number,
+  url: String,
+});
+
+const Image = mongoose.model('Image', imageSchema);
+Image.deleteMany().then(() => {
+  const fake = JSON.parse(fs.readFileSync('fake.json', 'utf8'));
+  const images = [];
+  for (let imageIndex = 0; imageIndex < fake.length; imageIndex += 1) {
+    const image = new Image(fake[imageIndex]);
+    images.push(image.save());
+  }
+  Promise.all(images).then(()=>{console.log('done'); process.exit()});
+});

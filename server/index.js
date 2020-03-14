beff1bdf34936b0c442cebe9d8a6e264f157f8d4
Peter Shoaf <32045721+DownsizeEngineering@users.noreply.github.com>
2020-03-13 17:17:52 -0700

    add api route to GET images by item ID

--- server/index.js
+++ server/index.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const path = require('path');
-
+const { getImagesByID } = require(path.resolve(__dirname, '..', 'database', 'db.js'));
 const app = express();
 const port = process.env.PORT || 3000;
 app.listen(port/* , () => { console.log('Listening on port ', port); } */);
 
 app.use(express.static(path.resolve(__dirname, '..', 'public')));
+
+app.get('/images/:id', (req, res) => {
+  getImagesByID(req.params.id, (err, data) => {
+    if (err) {
+      return res.sendStatus(404);
+    }
+    data = data.map((item)=>(item.url));
+    return res.send(data);
+  });
+});

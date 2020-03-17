2f8177c36208cc706bc015336c01879f7bdbe021
Peter Shoaf <32045721+DownsizeEngineering@users.noreply.github.com>
2020-03-16 22:57:47 -0700

    refactor App as a class component, adjust test to check that it is a class

--- client/__tests__/App.ts
+++ client/__tests__/App.ts
@@ -2,10 +2,23 @@ import React from 'react';
 import { shallow, mount, render } from 'enzyme';
 import App from '../components/App.jsx';
 
-describe('TEST', ()=>{
-  test('render', ()=>{
-    const wrapper = shallow(<App />);
-    const staticDefinition = (<div id='imageDisplayer'>hello world</div>);
-    expect(shallow(<App />).containsMatchingElement(staticDefinition)).toBe(true);
-  })
+const SampleData = [
+  "northerly-images.s3.amazonaws.com/clothing/black/hat/\
+  15f23779-c608-4d1a-b542-efbfc3946567.jpeg",
+  "northerly-images.s3.amazonaws.com/clothing/black/hat/\
+  1882c5a8-0dd8-43e5-8077-3434fe2e69c4.jpeg",
+  "northerly-images.s3.amazonaws.com/clothing/black/hat/\
+  3b1fbd0b-9fbf-414a-89eb-b3aa80955f26.jpeg",
+  "northerly-images.s3.amazonaws.com/clothing/black/hat/\
+  a7013e75-a700-4875-8ef0-af899946a3e0.jpeg",
+  "northerly-images.s3.amazonaws.com/clothing/black/hat/\
+  b2f134d0-0fc8-472d-8ae1-b1a9ee696182.jpeg"
+  ]
+
+describe('App Component', ()=>{
+  test('It should be a stateful class component', ()=>{
+   
+   expect(App.prototype.constructor.toString().includes('class App '))
+    .toBe(true);
+  });
 });
--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 
-const App = () => (
-    <div id='imageDisplayer'>
-      hello world
-    </div>
-);
+class App extends React.Component {
+  constructor (props) {super(props);}
+  render() { 
+    return (
+      <div id='imageDisplayer'>
+        hello world
+      </div>
+    );
+  }
+}
 
 export default App;

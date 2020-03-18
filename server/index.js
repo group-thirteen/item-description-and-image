972066b604185121c3831045c07f6d3811897986
Peter Shoaf <32045721+DownsizeEngineering@users.noreply.github.com>
2020-03-17 20:13:57 -0700

    add CORS policy to images/:id route

--- server/index.js
+++ server/index.js
@@ -2,13 +2,21 @@ const express = require('express');
 const path = require('path');
 const { getImagesByID } = require(
   path.resolve(__dirname, '..', 'database', 'db.js'));
+const cors = require('cors');
+
+const corsOptions = {
+  origin: 'http://localhost:3000',
+  methods: "GET",
+  optionsSuccessStatus: 200
+}
+
 const app = express();
 const port = process.env.PORT || 3000;
 app.listen(port/* , () => { console.log('Listening on port ', port); } */);
 
 app.use(express.static(path.resolve(__dirname, '..', 'public')));
 
-app.get('/images/:id', (req, res) => {
+app.get('/images/:id', cors(corsOptions), (req, res) => {
   getImagesByID(req.params.id, (err, data) => {
     if (err) {
       return res.sendStatus(404);

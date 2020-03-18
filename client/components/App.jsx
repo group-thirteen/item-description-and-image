1bbe646c94ce02bb29e9d50ce350dd6254748187
Peter Shoaf <32045721+DownsizeEngineering@users.noreply.github.com>
2020-03-17 20:16:30 -0700

    add fetch function, flesh out state, scaffold div body to show correct API call

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,14 +1,32 @@
 import React from 'react';
+import axios from 'axios';
+
+const server = process.env.SERVER || 'http://localhost:3000';
 
 class App extends React.Component {
   constructor (props) {
     super(props);
-    this.state = true;
+    this.state = {
+      urls: [],
+      currentIndex: 0,
+    };
+    this.fetch();
+    this.fetch = this.fetch.bind(this);
     }
+
+  fetch() {
+    axios.get(server.concat(`/images/${this.props.ID}`))
+    .then(response => {
+      const state = this.state;
+      this.state.urls = response.data;
+      this.setState(state);
+    });
+    
+  }
   render() { 
     return (
       <div id='imageDisplayer'>
-        hello world
+        hello {this.state.urls[this.state.currentIndex]}
       </div>
     );
   }

b34fe1033ed55ca11b806251f6dc1bfb4a57ef00
Peter Shoaf <32045721+DownsizeEngineering@users.noreply.github.com>
2020-03-18 18:42:08 -0700

    refactor fetch to async function to return axios.get ASAP, add flexbox styling to imageDisplayer div, use sample data instead of fetch until deployment

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,6 +1,21 @@
 import React from 'react';
 import axios from 'axios';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
+import Carousel from './Carousel.jsx';
+import Viewer from './Viewer.jsx';
+//  save on our AWS bill
+const sampleURLs = [
+  'localhost:3000/images/0.jpeg',
+  'localhost:3000/images/1.jpeg',
+  'localhost:3000/images/2.jpeg',
+  'localhost:3000/images/3.jpeg',
+  'localhost:3000/images/4.jpeg',
+];
+
+const FlexHoriz = styled.div`
+  display: flex;
+`;
 
 const server = process.env.SERVER || 'http://localhost:3000';
 
@@ -8,32 +23,27 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      urls: [],
+      urls: sampleURLs,
       currentIndex: 0,
     };
-    this.fetch();
+    // this.fetch();
     this.fetch = this.fetch.bind(this);
   }
 
-  fetch() {
-    axios.get(server.concat(`/images/${this.props.ID}`))
-      .then((response) => {
-        const { state } = this;
-        state.urls = response.data;
-        this.setState(state);
-      });
+  async fetch() {
+    const response = await axios.get(server.concat(`/images/${this.props.ID}`));
+    const { state } = this;
+    state.urls = response.data;
+    this.setState(state);
+    return response.data;
   }
 
   render() {
     return (
-      <div id='imageDisplayer'>
-        <span>
-          hello
-        </span>
-        <span>
-          {this.state.urls[this.state.currentIndex]}
-        </span>
-      </div>
+      <FlexHoriz id='imageDisplayer'>
+          <Carousel URLs={this.state.urls}/>
+          <Viewer image={this.state.urls[this.state.currentIndex]} />
+      </FlexHoriz>
     );
   }
 }

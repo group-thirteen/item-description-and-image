34ad5388a7f90a75e15ae55527ad2217874b4f08
Peter Shoaf <32045721+DownsizeEngineering@users.noreply.github.com>
2020-03-18 18:42:43 -0700

    rename target mountpoint

--- client/index.jsx
+++ client/index.jsx
@@ -2,4 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App.jsx';
 
-ReactDOM.render(<App ID={1}/>, document.getElementById('imageDisplayer'));
+ReactDOM.render(<App ID={2}/>, document.getElementById('displayerMountpoint'));

6d45880d662c063355d7f72b1b595c4f10902444
Peter Shoaf <32045721+DownsizeEngineering@users.noreply.github.com>
2020-03-20 22:39:44 -0700

    add Zoom functionaility with conditional render of modal box on Viewer click

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -4,6 +4,7 @@ import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import Carousel from './Carousel.jsx';
 import Viewer from './Viewer.jsx';
+import ZoomModal from './ZoomModal.jsx';
 //  save on our AWS bill
 const sampleURLs = [
   'localhost:3000/images/0.jpeg',
@@ -25,6 +26,7 @@ class App extends React.Component {
     this.state = {
       urls: sampleURLs,
       currentIndex: 0,
+      zoomed: false,
     };
     // this.fetch();
     this.fetch = this.fetch.bind(this);
@@ -38,6 +40,12 @@ class App extends React.Component {
     return response.data;
   }
 
+  toggleZoomed() {
+    const newState = this.state;
+    newState.zoomed = !newState.zoomed;
+    this.setState(newState);
+  }
+
   setCurrentIndexGen(i) {
     return () => {
       this.setState({ currentIndex: i });
@@ -46,11 +54,24 @@ class App extends React.Component {
   }
 
   render() {
+    let zoomModal;
+    if (this.state.zoomed) {
+      zoomModal = (
+        <ZoomModal toggle={this.toggleZoomed.bind(this)}
+        URLs = {this.state.urls}
+        setIndex={this.setCurrentIndexGen.bind(this)}
+        current={this.state.currentIndex}
+        />
+      );
+    }
+
     return (
       <FlexHoriz id='imageDisplayer'>
           <Carousel URLs={this.state.urls}
           setIndex={this.setCurrentIndexGen.bind(this)}/>
-          <Viewer image={this.state.urls[this.state.currentIndex]} />
+          <Viewer image={this.state.urls[this.state.currentIndex]}
+            toggle={this.toggleZoomed.bind(this)} />
+            {zoomModal}
       </FlexHoriz>
     );
   }

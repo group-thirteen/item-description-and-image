5684d01f41e7013212af2733b9bae839e31c4d7f
Peter Shoaf <32045721+DownsizeEngineering@users.noreply.github.com>
2020-03-22 15:13:41 -0700

    add newline after imports

--- client/components/ZoomModal.jsx
+++ client/components/ZoomModal.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import { XCircle } from '@styled-icons/boxicons-solid';
+
 const server = 'http://localhost:3000';
 
 const Modal = styled.div`

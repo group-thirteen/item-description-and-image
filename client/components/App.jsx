f0d7d3315e49807aa0c9af164d5c74f89482bb88
Peter Shoaf <32045721+DownsizeEngineering@users.noreply.github.com>
2020-03-22 12:34:58 -0700

    add shiftUp, shiftDown, and top so we can track which images should display in the carousel, set NumImages to a constant in case we want to display a number other than 5 images later

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -12,6 +12,9 @@ const sampleURLs = [
   'localhost:3000/images/2.jpeg',
   'localhost:3000/images/3.jpeg',
   'localhost:3000/images/4.jpeg',
+  'localhost:3000/images/5.jpeg',
+  'localhost:3000/images/6.jpeg',
+  'localhost:3000/images/7.jpeg',
 ];
 
 const FlexHoriz = styled.div`
@@ -19,6 +22,7 @@ const FlexHoriz = styled.div`
 `;
 
 const server = process.env.SERVER || 'http://localhost:3000';
+const numImages = 5;
 
 class App extends React.Component {
   constructor(props) {
@@ -27,6 +31,7 @@ class App extends React.Component {
       urls: sampleURLs,
       currentIndex: 0,
       zoomed: false,
+      top: 0,
     };
     // this.fetch();
     this.fetch = this.fetch.bind(this);
@@ -53,6 +58,26 @@ class App extends React.Component {
     };
   }
 
+  shiftDown() {
+    let index = this.state.top;
+    const distance = index + numImages >= this.state.urls.length
+      ? this.state.urls.length - 1 - index
+      : numImages;
+    index += distance;
+    this.setState({ top: index });
+    return distance;
+  }
+
+  shiftUp() {
+    let index = this.state.top;
+    const distance = index - numImages <= 0
+      ? index
+      : index - numImages;
+    index -= distance;
+    this.setState({ top: index });
+    return distance;
+  }
+
   render() {
     let zoomModal;
     if (this.state.zoomed) {
@@ -61,17 +86,24 @@ class App extends React.Component {
         URLs = {this.state.urls}
         setIndex={this.setCurrentIndexGen.bind(this)}
         current={this.state.currentIndex}
+        top={this.state.top}
         />
       );
     }
 
     return (
       <FlexHoriz id='imageDisplayer'>
-          <Carousel URLs={this.state.urls}
-          setIndex={this.setCurrentIndexGen.bind(this)}/>
+          <Carousel
+            URLs={this.state.urls}
+            setIndex={this.setCurrentIndexGen.bind(this)}
+            top={this.state.top}
+            shiftUp={this.shiftUp.bind(this)}
+            shiftDown={this.shiftDown.bind(this)}
+          />
           <Viewer image={this.state.urls[this.state.currentIndex]}
-            toggle={this.toggleZoomed.bind(this)} />
-            {zoomModal}
+            toggle={this.toggleZoomed.bind(this)}
+           />
+          {zoomModal}
       </FlexHoriz>
     );
   }

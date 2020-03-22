a79a29f488e45c7fd5dfd4fa1a10bee17810323d
Peter Shoaf <32045721+DownsizeEngineering@users.noreply.github.com>
2020-03-22 12:38:06 -0700

    wrap all images in 'imageSelect' div, set imgHeight to const

--- client/components/Carousel.jsx
+++ client/components/Carousel.jsx
@@ -1,17 +1,30 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
 
 function Carousel(props) {
+  const imgHeight = 75;
+  const offset = props.top * imgHeight;
+  const imgSelectStyle = {
+    transform: `translateY(-${offset}px)`,
+    overflow: 'hidden',
+    zindex: '-1',
+  };
+
   return (
-    <span id="Carousel" >
+    <div id="Carousel" >
+      <button onClick={props.shiftUp}/>
+      <div id ="imageSelect" style={imgSelectStyle}>
       {props.URLs.map((url, index) => (
         <div key={index}>
-          <img src={`http://${url}`} height="75"
+          <img src={`http://${url}`} height={`${imgHeight}px`}
               onClick={props.setIndex(index)}>
               </img>
         </div>
       ))}
-    </span>
+      </div>
+      <button onClick={props.shiftDown} />
+    </div>
   );
 }
 
@@ -19,6 +32,9 @@ Carousel.propTypes = {
   URLs: PropTypes.array.isRequired,
   current: PropTypes.number,
   setIndex: PropTypes.func.isRequired,
+  top: PropTypes.number,
+  shiftUp: PropTypes.func,
+  shiftDown: PropTypes.func,
 };
 
 export default Carousel;
